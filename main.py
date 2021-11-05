5dead5932a20f3974450eb85f1a59ae4b595efd5
scarpma <scarpma@gmail.com>
2021-11-05 14:59:41 +0100

    still not working

--- main.py
+++ main.py
@@ -4,7 +4,98 @@ import numpy as np
 from trainer import Trainer
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from dsets import *
 from model.voxel2mesh import Voxel2Mesh as network
+
+#import argparse
+#
+#parser = argparse.ArgumentParser()
+#parser.add_argument(
+#        '-proj',
+#        type=str,
+#        required=True,
+#        help='projection along which to slice the volumes'
+#)
+#parser.add_argument(
+#        '-batch_size',
+#        type=int,
+#        required=False,
+#        default=4,
+#        help='batch size'
+#)
+#parser.add_argument(
+#        '-workers',
+#        type=int,
+#        required=False,
+#        default=4,
+#        help='number of subprocesses to spawn to do preprocessing'
+#)
+#parser.add_argument(
+#        '-epochs',
+#        type=int,
+#        required=False,
+#        default=50,
+#        help='epochs'
+#)
+#parser.add_argument(
+#        '-in_channels',
+#        type=int,
+#        required=False,
+#        default=7,
+#        help='num of input channels of the network'
+#)
+#parser.add_argument(
+#        '-depth',
+#        type=int,
+#        required=False,
+#        default=7,
+#        help='depth of the network'
+#)
+#parser.add_argument(
+#        '-wf',
+#        type=int,
+#        required=False,
+#        default=3,
+#        help='2**wf is the num of filters in the first conv'
+#)
+#parser.add_argument(
+#        '-lr',
+#        type=float,
+#        required=False,
+#        default=0.0001,
+#        help='init learning rate'
+#)
+#parser.add_argument(
+#        '--shuffle',
+#        action='store_true',
+#        default=False,
+#        help=('shuffle the dataset every epoch')
+#)
+#
+#args = parser.parse_args()
+#
+#
+#restart_path = 0
+#restart_path = 'runs/models/v2_noaug_sagittal_suffle_64batch/'
+#
+#shuffle_bool = args.shuffle
+#num_workers = args.workers
+#batch_size = args.batch_size
+#epochs = args.epochs
+## in_channels = args.in_channels
+## depth = args.depth
+## wf = args.wf
+## lr = args.lr
+shuffle_bool = True
+num_workers = 2
+batch_size = 1
+epochs = 20
+
+run_name = 'prova1'
+
+print(args)
+
+
 
 
 def init(cfg):
@@ -44,32 +135,17 @@ print('Experiment ID: {}, Trial ID: {}'.format(cfg.experiment_idx,
                                                    trial_id))
 
 
-print("Create network")
-classifier = network(cfg)
-classifier.cuda()
-
-print("Initialize optimizer")
-optimizer = optim.Adam(filter(lambda p: p.requires_grad,
-                              classifier.parameters()),
-                       lr=cfg.learning_rate)
-
 print("Load pre-processed data")
-data_obj = cfg.data_obj
-data = data_obj.quick_load_data(cfg, trial_id)
+trn_dset = dsets.SegmentationDataset(10, isValSet_bool=False)
+val_dset = dsets.SegmentationDataset(10, isValSet_bool=True)
 
-loader = DataLoader(data[DataModes.TRAINING],
-                    batch_size=classifier.config.batch_size,
-                    shuffle=True)
-
-print("Trainset length: {}".format(loader.__len__()))
-
-print("Initialize evaluator")
-evaluator = Evaluator(classifier, optimizer, data, trial_path, cfg, data_obj)
+#print("Initialize evaluator")
+#evaluator = Evaluator(classifier, optimizer, data, trial_path, cfg, data_obj)
 
 print("Initialize trainer")
-trainer = Trainer(classifier, loader, optimizer, cfg.numb_of_itrs,
-                  cfg.eval_every, trial_path, evaluator)
+trainer = Trainer(classifier, trn_dset, val_dset, tb_prefix=run_name)
 
-trainer.train(start_iteration=epoch)
+trainer.train(epochs,batch_size,num_workers=num_workers, shuffle_bool=shuffle_bool, lr=lr)
+
 # To evaluate a pretrained model, uncomment line below and comment the line above
 # evaluator.evaluate(epoch)
